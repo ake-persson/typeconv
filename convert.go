e60d0b92dc649402daa61d73b5dc670aa0c9b9c5
Michael Persson <michael.ake.persson@gmail.com>
2018-05-07 18:59:43 -0500

    refactor

--- convert.go
+++ convert.go
@@ -3,67 +3,130 @@ package cnv
 import (
 	"reflect"
 	"strconv"
+	"time"
 )
 
 type Converter interface {
-	Interface() (interface{}, error)
+	Parse() (interface{}, error)
 }
+
+func ParseBool(s string) (bool, error) {
+	return strconv.ParseBool(s)
+}
+
+func ParseInt(s string) (int, error) {
+	i, err := strconv.ParseInt(s, 10, 0)
+	return int(i), err
+}
+
+func ParseInt8(s string) (int8, error) {
+	i, err := strconv.ParseInt(s, 10, 8)
+	return int8(i), err
+}
+
+func ParseInt16(s string) (int16, error) {
+	i, err := strconv.ParseInt(s, 10, 16)
+	return int16(i), err
+}
+
+func ParseInt32(s string) (int32, error) {
+	i, err := strconv.ParseInt(s, 10, 32)
+	return int32(i), err
+}
+
+func ParseInt64(s string) (int64, error) {
+	i, err := strconv.ParseInt(s, 10, 64)
+	return int64(i), err
+}
+
+func ParseUint(s string) (uint, error) {
+	i, err := strconv.ParseUint(s, 10, 0)
+	return uint(i), err
+}
+
+func ParseUint8(s string) (uint8, error) {
+	i, err := strconv.ParseUint(s, 10, 8)
+	return uint8(i), err
+}
+
+func ParseUint16(s string) (uint16, error) {
+	i, err := strconv.ParseUint(s, 10, 16)
+	return uint16(i), err
+}
+
+func ParseUint32(s string) (uint32, error) {
+	i, err := strconv.ParseUint(s, 10, 32)
+	return uint32(i), err
+}
+
+func ParseUint64(s string) (uint64, error) {
+	i, err := strconv.ParseUint(s, 10, 64)
+	return uint64(i), err
+}
+
+func ParseFloat32(s string) (float32, error) {
+	i, err := strconv.ParseFloat(s, 32)
+	return float32(i), err
+}
+
+func ParseFloat64(s string) (float64, error) {
+	i, err := strconv.ParseFloat(s, 64)
+	return float64(i), err
+}
+
+func ParseTime(s string) (time.Time, error) {
+	return time.Parse(time.RFC3339, s)
+}
+
+// Parse with interface{} pointer?
 
 func Parse(s string, kind reflect.Kind) (interface{}, error) {
 	switch kind {
 	case reflect.Bool:
-		return strconv.ParseBool(s)
+		return ParseBool(s)
 
 	case reflect.Int:
-		i, err := strconv.ParseInt(s, 10, 0)
-		return int(i), err
+		return ParseInt(s)
 	case reflect.Int8:
-		i, err := strconv.ParseInt(s, 10, 8)
-		return int8(i), err
+		return ParseInt8(s)
 	case reflect.Int16:
-		i, err := strconv.ParseInt(s, 10, 16)
-		return int16(i), err
+		return ParseInt16(s)
 	case reflect.Int32:
-		i, err := strconv.ParseInt(s, 10, 32)
-		return int32(i), err
+		return ParseInt32(s)
 	case reflect.Int64:
-		i, err := strconv.ParseInt(s, 10, 64)
-		return int64(i), err
+		return ParseInt64(s)
 
 	case reflect.Uint:
-		i, err := strconv.ParseUint(s, 10, 0)
-		return uint(i), err
+		return ParseInt(s)
 	case reflect.Uint8:
-		i, err := strconv.ParseInt(s, 10, 8)
-		return uint8(i), err
+		return ParseInt8(s)
 	case reflect.Uint16:
-		i, err := strconv.ParseInt(s, 10, 16)
-		return uint16(i), err
+		return ParseInt16(s)
 	case reflect.Uint32:
-		i, err := strconv.ParseInt(s, 10, 32)
-		return uint32(i), err
+		return ParseInt32(s)
 	case reflect.Uint64:
-		i, err := strconv.ParseInt(s, 10, 64)
-		return uint64(i), err
+		return ParseInt64(s)
 
 	case reflect.Float32:
-		f, err := strconv.ParseFloat(s, 32)
-		return float32(f), err
+		return ParseFloat32(s)
 	case reflect.Float64:
-		f, err := strconv.ParseFloat(s, 64)
-		return float64(f), err
+		return ParseFloat64(s)
 
 	case reflect.String:
 		return s, nil
 
-		//	case reflect.Struct:
-		//		if va.Type().String() == "time.Time" && vb.Type().String() == "time.Time" {
-		//			return va.Interface().(time.Time).Equal(vb.Interface().(time.Time)), nil
-		//		}
+	case reflect.Struct:
+		/*
+			if kind == "time.Time" {
+				return ParseTime(s)
+			}
+		*/
 
-		//	if ca, ok := va.Interface().(Comparer); ok {
-		//		return ca.Eq(vb.Interface())
-		//	}
+		/*
+			if c, ok := s.(Converter); ok {
+				return c.Parse(s)
+			}
+		*/
 	}
 
 	return nil, ErrKindNotSupported
